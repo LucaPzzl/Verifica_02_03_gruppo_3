acad7198140af37c6027770061a9085934d6a1f2
LucaPzzl <119593629+LucaPzzl@users.noreply.github.com>
2023-03-02 12:19:53 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import saluto_roberto_michelotto
+import saluto_mattia_tiepo
+import saluto_mattia_todisco
+
+def saluto_nome_cognome():
+    print("Salve Luca Pezzolati")
+print(saluto_nome_cognome())
+
+main.saluto_roberto_michelotto.saluto_nome_cognome()
+main.saluto_mattia_tiepo.saluto_nome_cognome()
+main.saluto_mattia_todisco.saluto_nome_cognome()
